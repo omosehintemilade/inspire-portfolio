757885ccbf41933cd58712f62b7ff355f539c24a
Inspire <emmaexcel0@gmail.com>
2023-04-05 08:31:52 +0100

    removed jquery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import $ from "jquery";
 import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -9,7 +8,6 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 class App extends Component {
-
   constructor(props) {
     super();
     this.state = {
@@ -19,32 +17,15 @@ class App extends Component {
     };
   }
 
- 
-  
-
- 
   render() {
     return (
       <div>
-        <Header sharedData={this.state.sharedData.basic_info} />
-     
-        <About
-          resumeBasicInfo={this.state.resumeData.basic_info}
-          sharedBasicInfo={this.state.sharedData.basic_info}
-        />
-        <Projects
-          resumeProjects={this.state.resumeData.projects}
-          resumeBasicInfo={this.state.resumeData.basic_info}
-        />
-        <Skills
-          sharedSkills={this.state.sharedData.skills}
-          resumeBasicInfo={this.state.resumeData.basic_info}
-        />
-        <Experience
-          resumeExperience={this.state.resumeData.experience}
-          resumeBasicInfo={this.state.resumeData.basic_info}
-        />
-        <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
+        <Header />
+        <About />
+        <Projects />
+        <Skills />
+        <Experience />
+        <Footer />
       </div>
     );
   }
