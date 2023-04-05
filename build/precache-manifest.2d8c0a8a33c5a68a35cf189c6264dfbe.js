4fdfead4b77eba78713a82e85abb4f1a1e31ac96
Inspire <emmaexcel0@gmail.com>
2023-04-05 08:35:31 +0100

    pushed build to github

---
+++ build/precache-manifest.2d8c0a8a33c5a68a35cf189c6264dfbe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ac57b5e8e070b3d6675ca74fd446d2",
+    "url": "/Inspire0/my-portfolio/index.html"
+  },
+  {
+    "revision": "8b806cb61d355a9e32ed",
+    "url": "/Inspire0/my-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d64432e56eac897edfd9",
+    "url": "/Inspire0/my-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "8b806cb61d355a9e32ed",
+    "url": "/Inspire0/my-portfolio/static/js/2.2c6b39de.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Inspire0/my-portfolio/static/js/2.2c6b39de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64432e56eac897edfd9",
+    "url": "/Inspire0/my-portfolio/static/js/main.9898c9c5.chunk.js"
+  },
+  {
+    "revision": "f0499e87964eb775f09d",
+    "url": "/Inspire0/my-portfolio/static/js/runtime-main.7e284ba6.js"
+  },
+  {
+    "revision": "48ad761c853b3938fee6cf1264d514a0",
+    "url": "/Inspire0/my-portfolio/static/media/myProfile.48ad761c.jpg"
+  },
+  {
+    "revision": "65cec7163908fd22ffdb98d20f95753b",
+    "url": "/Inspire0/my-portfolio/static/media/p1.65cec716.png"
+  },
+  {
+    "revision": "6ed06583aa3c21ac9aeb67269b5dfceb",
+    "url": "/Inspire0/my-portfolio/static/media/p2.6ed06583.png"
+  },
+  {
+    "revision": "4b5f88681b18fa5fb8e15c1adf80cc3e",
+    "url": "/Inspire0/my-portfolio/static/media/p3.4b5f8868.png"
+  }
+]);
